420649d0277b662fac7f3123e021813ac2585d2a
Geun Woo, Shin <encyphered@gmail.com>
2012-08-07 17:39:49 +0900

    add some hosts and increase version

--- beforeLoad.js
+++ beforeLoad.js
@@ -3,7 +3,7 @@
 
     var host = window.location.host;
     if (!!host) {
-        var targetHosts = ['.kr', 'naver.com', 'daum.net', 'nate.com', 'tistory.com', 'egloos.com'];
+        var targetHosts = ['.kr', 'naver.com', 'daum.net', 'nate.com', 'tistory.com', 'egloos.com', 'career.co.kr'];
         for (var i in targetHosts) {
             if (host.indexOf(targetHosts[i]) >= 0) {
                 safari.self.tab.dispatchMessage('forceDefaultFont');
